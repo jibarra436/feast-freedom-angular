77495e8944092a79a59850029a0a5d076f83d5c9
Joey Ibarra <joeyibarra436@gmail.com>
2022-09-14 11:17:22 -0500

    Auto login after registration

--- src/app/createuser/createuser.component.ts
+++ src/app/createuser/createuser.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from '../services/api.service';
 import { FeastUser } from '../models/feastuser';
+import { UserloginComponent } from '../userlogin/userlogin.component';
 
 @Component({
   selector: 'app-createuser',
@@ -13,7 +14,7 @@ export class CreateuserComponent implements OnInit {
 
   us: FeastUser
 
-  constructor(private uservice: ApiService) {
+  constructor(private uservice: ApiService, private router: Router) {
     this.us = new FeastUser();
   }
 
@@ -22,12 +23,38 @@ export class CreateuserComponent implements OnInit {
 
   submitted = false;
 
+  login(user: FeastUser, apiService: ApiService, router: Router){
+    console.log("Logging in: "+user.email);
+    var credentials: any =
+    {
+      "username": user.email,
+      "password": user.password
+    }
+    var loginfo = <JSON>credentials;
+        
+    apiService.auth(loginfo, credentials.username).subscribe(response => {
+      localStorage.setItem('loggedIn', "true");
+      localStorage.setItem('token', response['token']);
+
+      apiService.getUser(credentials.username).subscribe(response => {
+        localStorage.setItem('userId', response.id.toString());
+        localStorage.setItem('userEmail', response.email);
+        localStorage.setItem('userFirst', response.firstName);
+        localStorage.setItem('userLast', response.lastName);
+      });
+    });
+
+    router.navigate([''])
+  }
+
   createuser(){
     console.log("Creating: "+this.us);
     this.submitted = true;
     this.uservice.saveuser(this.us).subscribe(response => {
       console.log(response);
     });
+    
+    setTimeout(this.login, 1000, this.us, this.uservice, this.router);
   }
 
 }
